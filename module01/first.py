5224ea4aa5b91b32071988b91fa9d24f26f58660
Maruf Mobin <mobinh264@gmail.com>
2022-10-09 12:59:22 +0600

    comment larging

--- module01/first.py
+++ module01/first.py
@@ -1,2 +1,13 @@
-print("Kopa Samsu")
+# printing output 
+# another line of comment 
+# I am larning python
+# Ctrl + /
+# alt + shift + a
+
+print("Kopa Samsu") # same line commnet 
 print("maruf")
+
+""" 
+    Large Mutiline comment 
+    anoter line of commnet 
+"""
