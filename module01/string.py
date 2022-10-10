e9bf272f9e99bdb81ba8e2e8f1265208fd547a9c
Maruf Mobin <mobinh264@gmail.com>
2022-10-10 11:17:14 +0600

    1-6 Introduction To String Type Variable

---
+++ module01/string.py
@@ -0,0 +1,12 @@
+name = "Akbar Ali"
+school = "Abu jhor Giffari"
+district = 'new Califonia'
+first_name = 'Ali'
+last_name = 'Akbar'
+full_name = first_name + " " + last_name
+hobby = 'To play Football \nand watch Cricket \nsleep at night '
+
+# print(full_name)
+# print(district)
+# print(name)
+print(hobby)
