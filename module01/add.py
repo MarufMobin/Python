5d55ffab5a311ca8a8e1782e1995359d2d23102f
Maruf Mobin <mobinh264@gmail.com>
2022-10-10 12:31:31 +0600

    1-8 Take User Input And Convert To Int For Operation

---
+++ module01/add.py
@@ -0,0 +1,10 @@
+num1 = input("Tell us the first Number : ")
+num2 = input("Tell us the second Number : ")
+num1_int = int(num1)
+num2_int = int(num2)
+# result = num1 + num2
+# print(result)
+# print(num1, num2)
+
+result = num1_int + num2_int
+print(result)
---
+++ module01/input.py
@@ -0,0 +1,9 @@
+# input()
+# input('Tell me your age: ')
+name = input('Dude, What is your name : ')
+age = input('Tell us about your age : ')
+print('I Can take input now.')
+# print(age)
+# print(type(age))
+print(name, age)
+
