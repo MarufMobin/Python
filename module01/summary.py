981c26dfbdd7d448279db85686e6e5fdff357dc1
Maruf Mobin <mobinh264@gmail.com>
2022-10-10 12:53:10 +0600

    module 1 summary

---
+++ module01/summary.py
@@ -0,0 +1,28 @@
+""" 
+    1. What is python
+    2. how to install python
+    3. VS Code ( python , Code runner )
+    4. Variable 
+    5. int 
+    6. string 
+    7. boolean
+    8. type
+    9. print
+    10. f string
+    11. input
+    12. Comment     
+
+"""
+
+""" 
+Home Work
+    1. three input from the use
+    2. add the three input and display the sum
+    3. take two numbers the show the multiplication
+    4. take first name and Last name and show full name
+    5. thake three inputs and output will be like :
+    " Hey I am ___. My age is __. I love to eat __.
+    6. take one input and display 4 time of that number 
+    7. take degree celcius and convert the temprature farnhite 
+
+"""
