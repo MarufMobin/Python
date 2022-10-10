af2bbf7daf9a37851e0735d51142566a7ec03edd
Maruf Mobin <mobinh264@gmail.com>
2022-10-10 11:36:44 +0600

    1-7 Boolean Variable, Check Data Types And F String

---
+++ module01/type.py
@@ -0,0 +1,20 @@
+weight = 58
+weight_type  = type(weight)
+print(weight_type)
+
+price = 13.7
+price_type = type(price)
+print(price_type)
+
+pen = "ECONO"
+print(type(pen))
+
+kanamachi = True
+print(type(kanamachi))
+
+person = 'Kodom Ali'
+age = 45
+# about_me = 'My name is' + person + age
+# F String Dynamic String make using this f string approch
+about_me = f'My name is {person}. My age is {age}.'
+print(about_me)
