43e0d6a270e966d302eae90f4d75c9fd5181eb5a
Maruf Mobin <mobinh264@gmail.com>
2022-10-11 20:38:17 +0600

    2-4 Understanding multiple conditions and nested conditions

---
+++ module02/multi_condition.py
@@ -0,0 +1,27 @@
+exam = 'Bank'
+passed  = True
+second = 'Bank'
+
+# using AND Oparator 
+
+# if exam == 'BCS' and passed == 'True' :
+#     print('Congrates ): ')
+#     print('You are selected the Job')
+# else :
+#     print('Try Next Time again')
+
+
+#  using OR oparation 
+if exam == 'BCS' or second == 'Bank' :
+    print('You are earn a great Oportunity')
+else :
+    print('try more and more then you are succcess')
+
+# IF ELSE Condition 
+# if exam == 'BCS' :
+#     print('You are Selected for the Police Job.')
+
+# if passed == True :
+#     print('Congrats')
+# else :
+#     print('Try next time again')
---
+++ module02/nested_condition.py
@@ -0,0 +1,10 @@
+salary = 32000
+has_flat = True
+
+if salary > 35000 :
+    if has_flat == True:
+        print('OK . You are a well Bride')
+    else:
+        print('First you Bought a flat')
+else:
+    print('firstly you earn more money the come for wedding')
