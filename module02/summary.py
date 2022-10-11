685fb2a684d97d82776eae045c359a2bd9d98d91
Maruf Mobin <mobinh264@gmail.com>
2022-10-12 01:49:55 +0600

    module 2 Summary Module and quiz also Homework

---
+++ module02/summary.py
@@ -0,0 +1,65 @@
+""" 
+    1. Compare ( < , > , <= , >= , ==, != )
+    2. Conditionals ( if, else , elif , nested if else )
+    3. Loop ( while , for )
+    4. break & Continue 
+
+    # Home work
+    1. print out odd numbers between 13 to 39
+    2. use for loop to print numbers between 13 to 39 using and range 
+    3. ( Challenging ) print out 20 to 1 using while loop ( reverse way )
+
+"""
+#  Module 2 Quiz
+
+# Not Undestand 
+# price = -10
+# if price :
+#     print("Yes")
+# else :
+#     print("No")
+
+# a=0
+# if a>0:
+#     print("Positive")
+# elif a<0:
+#     print("Negative")
+# else :
+#     print("Non negative")
+
+# i=1
+# while(i<3):
+#     print(i,end=" ")
+#     i+=1
+
+# i=1
+# while(i<10):
+#     print(i,end=" ")
+#     i+=1
+#     if i==5:
+#         break
+
+# i=1
+# while(i<10):
+#     print(i,end=" ")
+#     if i==5:
+#         break
+#     i+=1
+
+# for i in range(10): 
+#     if i==5:
+#         continue
+#     print(i,end=" ")
+
+# a="Bd"
+# for i in range(len(a)):
+#     print(a[i],end="")
+
+# a="Bd"
+# for i in range(len(a)):
+#     print(i,end="")
+
+# i=1
+# while(i<3):
+#     print(i)
+#     i+=1
