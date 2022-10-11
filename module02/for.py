4abbdf9be133e648bc1d0c95ba45dd9e675fa9c5
Maruf Mobin <mobinh264@gmail.com>
2022-10-12 01:32:07 +0600

    2-8 A Simple Introduction to for loop

---
+++ module02/for.py
@@ -0,0 +1,20 @@
+country = 'Bangladesh'
+
+# 1 No. Case Which is all element access for that existing string 
+
+# for letter in country:
+#     print(letter)
+
+# 2 No. case 0 to 4 run the Loop 
+# for num in range(5):
+#     print(num)
+
+# 3 No. Case there are we are passing 2 parameter first are starting index and second are ending - 1 index 
+
+# for index in range( 6, 13 ):
+#     print(index)
+
+# 4 No. Case there are we passed 3 parameter in range function. first parameter are starting index and second are ending index and third are incriment number 
+
+for n in range( 5, 45 , 5 ):
+    print(n)
