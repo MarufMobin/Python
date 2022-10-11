534e5b594e63f1d6e74895d2caf0a84af6bb20e2
Maruf Mobin <mobinh264@gmail.com>
2022-10-11 18:55:10 +0600

    2-3 Practice conditionals if elif else

---
+++ module02/conditionals.py
@@ -0,0 +1,21 @@
+# ( if & else uses )
+
+# biryani_price = 350
+
+# if biryani_price < 150 :
+#     print('Eating Biriyani ')
+# else :
+#     print('Butter Bon khabo Cha diye')
+
+
+# ( if & else if and else uses )
+
+biryani_price = 130
+kabab_price = 40
+
+if biryani_price < 150 :
+    print('Eating Biriyani ')
+elif kabab_price < 50 : 
+    print('Kabab Khabo')
+else :
+    print('Butter Bon khabo Cha diye')
