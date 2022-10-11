bdb38d44543755c61fedd5b9e64f6f4211c05277
Maruf Mobin <mobinh264@gmail.com>
2022-10-11 21:19:48 +0600

    2-5 How to write a while loop

---
+++ module02/while.py
@@ -0,0 +1,16 @@
+num = 1
+target = int(input('How Many Loop run : '))
+while num <= target :
+        print(f'I am Sorry {num}')
+        num = num + 1
+
+
+number = 0
+sum_target = int(input('Give me a Number the i will sum for all of them : '))
+ans = 0
+
+while number <= sum_target :
+    ans += number 
+    number = number + 1
+
+print(f'Your Total Number Sum is {ans}')    
