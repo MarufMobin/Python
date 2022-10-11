4aadee81a5c674cb30a43c22613cd0b2d0b11cb1
Maruf Mobin <mobinh264@gmail.com>
2022-10-11 13:23:42 +0600

    2-1 Recap and Module Introduction

---
+++ module01/practice.py
@@ -0,0 +1,53 @@
+""" 1. three input from the use
+2. add the three input and display the sum
+"""
+""" num1 = input('This is first input : ')
+num2 = input('This is second input : ')
+num3 = input('This is third input : ')
+
+num1_int = int(num1)
+num2_int = int(num2)
+num3_int = int(num3)
+
+sum = num1_int + num2_int + num3_int
+print(sum)
+ """
+#  3 take two numbers the show the multiplication 
+""" num4 = input("Please Give a Number : ")
+num5 = input("Please Give the second Number : ")
+multi = int(num4) * int(num5)
+print(multi) """
+
+
+# 4. take first name and Last name and show full name
+""" 
+first_name = input('Please Write your First Name : ')
+last_name = input('Please Write your Last Name : ')
+
+full_name = f'{first_name} {last_name}'
+print(full_name)
+
+"""
+
+# 5. thake three inputs and output will be like : 
+# " Hey I am ___. My age is __. I love to eat __.
+
+""" 
+name = input('Enter Your Name : ')
+age = input('Enter Your Age : ')
+food_name = input('Enter your Favourate Food Name : ')
+
+printing_taxt = f'Hey I am  {name}. My age is {age}. I love to eat {food_name}.'
+print(printing_taxt) 
+
+"""
+
+# 6. take one input and display 4 time of that number 
+
+# num = int(input('Enter A Number : '))
+# print(pow( 4, num ))
+
+# 7. take degree celcius and convert the temprature Fahrenheit   
+cel = int(input('Give me a Celcius : '))
+temp = cel * 9 / 5 + 32
+print(temp , "Degree Fahrenheit ")
---
+++ module02/recap.py
@@ -0,0 +1,23 @@
+price = 8000
+#  This is a Comment 
+country = 'Bangladesh'
+is_rich = True
+# print(is_rich)
+# print(type(is_rich))
+
+#  Naming Conversion in Variable 
+""" 
+    -> with out Number 
+    -> Double couts single couts not accepted 
+    -> not support any kind of special carected without ( _ ) underscore
+"""
+""" 
+    Knowing Consepts 
+    -> Variable
+    -> Variable types : type() 
+    -> output : print() 
+    -> input : input()
+    -> int : int()
+"""
+district = input('Where do you live : ')
+print(f'This person lives in {district}')
