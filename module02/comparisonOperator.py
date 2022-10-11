0203f36a0ab950ab4c78aebc9029fdcdf3fa8d13
Maruf Mobin <mobinh264@gmail.com>
2022-10-11 13:44:44 +0600

    2-2 Explore six types of comparison operator

---
+++ module02/comparisonOperator.py
@@ -0,0 +1,25 @@
+#  6 type of Comparison 
+
+# < cheks whether the left site of the symbol is smaller then the right side
+# print( 12 < 35 )
+# print( 42 < 35 )
+
+#  > ( checks bigger or not )
+# print( 12 > 56 )
+# print( 89 > 12 )
+
+# <= ( checks whether smaller or equal )
+# print( 12 >= 12 )
+
+# >= ( checks whether bigger or equal )
+# print( 12 <= 12 )
+
+my_money = 45
+your_money = 65
+
+# == checks whether both the values are equal
+print( my_money == your_money )
+
+# != Checks whether both values are not equal
+print( my_money != your_money )
+
