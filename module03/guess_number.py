eaa54c99dff784654fac4beec53d986cbca25060
Maruf Mobin <mobinh264@gmail.com>
2022-10-13 01:50:58 +0600

    3-4 Guess a number

---
+++ module03/guess_number.py
@@ -0,0 +1,18 @@
+""" 
+    Game :  Guess a Number 
+    Requirement 
+    * user instractions
+    * delay 
+
+    formula = ( (2x+8)/2 ) - x
+"""
+from time import sleep
+
+x = input("Guess a Number : ")
+x = input("Multiply 2 with your Number : ")
+x = input("Add 8 with your result : ")
+x = input("divided the whole result with 2 : ")
+x = input("Substract the guessed number with the result : ")
+print("Your guess number 4\n")
+
+
