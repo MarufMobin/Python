cd9dc5958654eeb5a839a3870bf610e26f89411d
Maruf Mobin <mobinh264@gmail.com>
2022-10-13 19:48:19 +0600

    4-6 Build In Modules And Build In Functions

---
+++ module04/build_in_modules.py
@@ -0,0 +1,19 @@
+# import math -> sob gulow dhore dhore ber kra use krte hbe math.ceil()  <-- ai bhabe
+
+from math import sin, ceil
+
+# result = sin(30)
+result = ceil(2.13)
+# print(result)
+
+import random 
+
+num = random.random()
+num = random.randint( 70, 90 )
+winner = random.choice( [12, 15, 4, 63, 52, 55, 8 ] )
+# print(num)
+print(winner)
+
+import datetime as dt 
+
+print(dt.date)
