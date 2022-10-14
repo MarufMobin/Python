5941490e5c0b1c3ef9b9d8a3e07ea88f425e7558
Maruf Mobin <mobinh264@gmail.com>
2022-10-14 18:00:19 +0600

    5-3 Explore Dictionary and Dictionary Methods

---
+++ module05/dictionary.py
@@ -0,0 +1,18 @@
+# numbers = [ 12, 45, 56, 12, 89 ]
+
+marks = { 'physics': 12, 'chemistry': 45, 'math': 56 }
+marks['math'] = 56.5
+marks['english'] = 89
+
+# del marks['chemistry']
+print(marks)
+
+marks_keys = marks.keys()
+marks_values = marks.values()
+# marks.clear()
+marks_items = marks.items()
+# print(marks_items)
+# print(marks)
+# print(marks_values)
+# print(marks_keys)
+
