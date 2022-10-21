92089512ff6d97520ddb87a3cbdb639592349991
Maruf Mobin <mobinh264@gmail.com>
2022-10-21 18:40:03 +0600

    9-2 add you own operator and magic methods

---
+++ module09/RealDunder.py
@@ -0,0 +1,44 @@
+# dunder 
+# magic method
+# special method
+
+class Person:
+
+    def __init__( self, name, age , money , height = 65 ):
+        self.name = name
+        self.age = age
+        self.money = money
+        self.height = height
+    
+    def __call__(self):
+       return f'This is {self.name} with age {self.age} and have {self.money}' 
+
+    def __add__( self, other ):
+        # return self.money + other.money
+        # return self.name + other.name
+        return self.age + other.age
+
+    def __eq__(self, other ) :
+        return self.age == other.age
+
+    def __len__(self):
+        return self.height
+
+maruf = Person( 'Maruf Mobin', 22, 300 , 68 )
+jabbar = Person('Abdul Jabbar', 21, 400 )
+
+# print(maruf.name +' '+ jabbar.name)
+# print(maruf.age + jabbar.age)
+# print(maruf.money + jabbar.money
+# print(maruf.money)
+reply = maruf()
+print(reply)
+
+print( 'Compare to Objects : ', maruf == jabbar )
+
+
+friends = [45, 65, 98, 12,32]
+# print(len(friends))
+print(len(maruf))
+print(len(jabbar))
+
