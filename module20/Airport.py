a006fa599f88da9cf5742de36ac3dc099324fd8a
Maruf Mobin <mobinh264@gmail.com>
2022-11-13 02:15:21 +0600

    20-4 Create airports from the loaded data

--- module20/Airport.py
+++ module20/Airport.py
@@ -1,7 +1,8 @@
 class Airport:
-    def __init__( self, code, name, country, lat, long, rate ) -> None:
+    def __init__( self, code, name, city, country, lat, long, rate ) -> None:
         self.code = code 
         self.name = name
+        self.city = city
         self.country = country
         self.lat = lat
         self.long = long 
--- module20/All_Airports.py
+++ module20/All_Airports.py
@@ -1,15 +1,23 @@
 import csv
+from Airport import Airport 
 
 class AllAirports:
     def __init__(self) -> None:
+        self.airports = None
         self.load_airports_data('./data/airport.csv')
 
     def load_airports_data( self, file_path ):
-        with open(file_path,encoding="utf8") as file:
-            airports = csv.reader(file)
+        airports = {}
+        with open(file_path, 'r', encoding="utf8") as file:
+            lines = csv.reader(file)
 
-            for airport in airports:
+            for line in lines:
+                airports[line[4]] = Airport( line[4], line[1], line[2], line[3], line[6], line[7], 0 )
+            
+            self.airports = airports
+            for airport in airports.items():
                 print(airport)
                 
+
 
 AllAirports()
