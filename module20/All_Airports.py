027d6a83aa3a7fbbd7f04d7021a5b84e23a2ceb2
Maruf Mobin <mobinh264@gmail.com>
2022-11-13 02:40:26 +0600

    20-5 Add Conversion rate with the airport

--- module20/All_Airports.py
+++ module20/All_Airports.py
@@ -8,12 +8,39 @@ class AllAirports:
 
     def load_airports_data( self, file_path ):
         airports = {}
+        currency_rate = {}
+        country_currency = {}
+
+        # get Currency name <-----> rate 
+        with open('./data/currencyrates.csv', 'r') as file:
+            lines = csv.reader(file)
+            for line in lines:
+                currency_rate[line[1]] = line[2]
+        file.close()
+        
+        # dictionary country <-----> Currency name 
+        with open('./data/countrycurrency.csv', 'r') as file:
+            lines = csv.reader(file)
+            # header = next(lines)
+            for line in lines:
+                country_currency[line[0]] = line[1]
+        file.close()
+
+
+        # Create Airport are here
         with open(file_path, 'r', encoding="utf8") as file:
             lines = csv.reader(file)
 
-            for line in lines:
-                airports[line[4]] = Airport( line[4], line[1], line[2], line[3], line[6], line[7], 0 )
-            
+            try:
+                for line in lines:
+                    country = line[3]
+                    currency = country_currency[country]
+                    rate = currency_rate[currency]
+
+                    airports[line[4]] = Airport( line[4], line[1], line[2], line[3], line[6], line[7], rate )
+            except KeyError as e:
+                print(e)
+          
             self.airports = airports
             for airport in airports.items():
                 print(airport)
