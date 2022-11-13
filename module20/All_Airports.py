784d257f65d5678e4e619432aabd2aea799eaa13
Maruf Mobin <mobinh264@gmail.com>
2022-11-13 22:58:49 +0600

    20-7 Calcualte Fare (not accurate) based on distance and rate

--- module20/All_Airports.py
+++ module20/All_Airports.py
@@ -40,11 +40,11 @@ class AllAirports:
 
                     airports[line[4]] = Airport( line[4], line[1], line[2], line[3], line[6], line[7], rate )
             except KeyError as e:
-                print(e)
+                print('Key not found',e)
           
             self.airports = airports
-            for airport in airports.items():
-                print(airport)
+            # for airport in airports.items():
+            #     print(airport)
                 
     def get_distance_between_two_airports( self, lat1, lon1, lat2, lon2 ):
         radius = 6371
@@ -58,5 +58,19 @@ class AllAirports:
         c = 2 * atan2(sqrt(a), sqrt(1 - a))
         distance = radius * c
         return distance
+    
+    def distance_between_two_airports( self, airport1_code , airport2_code ):
+        airport1 = self.airports[airport1_code]
+        airport2 = self.airports[airport2_code]
+        distance = self.get_distance_between_two_airports( airport1.lat, airport1.lon, airport2.lat, airport2.lon )
+        return distance 
 
-AllAirports()
+    def get_ticket_price( self, start , end ):
+        distance = self.distance_between_two_airports( start, end )
+        airport1 = self.airports[start]
+        fare = distance * airport1.rate
+        return fare
+
+world_tour = AllAirports()
+fare = world_tour.get_ticket_price('DAC', 'PRA')
+print('Ticket Fare',fare)
