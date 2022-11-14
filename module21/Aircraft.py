ddcb81de632328fb119f6e583500e2a596741cac
Maruf Mobin <mobinh264@gmail.com>
2022-11-14 20:12:35 +0600

    20-1 Module Introduction and previous module recap Also Hole previus Code Coppied

---
+++ module21/Aircraft.py
@@ -0,0 +1,17 @@
+class Aircraft:
+    def __init__(self, make , code , typ, flight_range ) -> None:
+        self.make = make
+        self.code = code 
+        self.type = typ
+        self.flight_range = flight_range
+
+    def get_make( self ):
+        return self.make
+
+    def get_flight_range( self ):
+        return self.flight_range 
+
+    def __repr__(self) -> str:
+        return f' Aircraft Make: {self.make} Code: {self.code} type: {self.type} range: {self.flight_range}'
+    
+
