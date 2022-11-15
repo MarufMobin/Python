3ece297e38b7981ce37ba73a334cd3e235ef1d99
Maruf Mobin <mobinh264@gmail.com>
2022-11-15 18:54:48 +0600

    21-2 Create Trave Agent and multiple trip options

--- module21/Trip.py
+++ module21/Trip.py
@@ -2,5 +2,11 @@ class Trip:
     def __init__(self, trip_cities, start_date ) -> None:
         self.trip_cities = trip_cities
         self.start_date = start_date
+        self.aircraft = None
+        self.trip_route = None 
+        self.cost = None
+
+    def  __repr__(self) -> str:
+        return f'Trip: {self.trip_cities} Aircraft: {self.aircraft} Route: {self.trip_route} Cost: {self.cost}'
     
     
