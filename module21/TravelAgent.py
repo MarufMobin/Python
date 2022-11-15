d8d896f14a475b6222410daaa093b705a24a5db7
Maruf Mobin <mobinh264@gmail.com>
2022-11-15 22:07:18 +0600

    21-6 Fixed Route Multi City Trip Plan 2-1

--- module21/TravelAgent.py
+++ module21/TravelAgent.py
@@ -1,5 +1,7 @@
 from All_Airports import AllAirports
 from Air_Lines import AirLines
+from Trip import Trip
+
 
 class TravelAgent:
     def __init__(self, name) -> None:
@@ -23,13 +25,25 @@ class TravelAgent:
         price = self.all_airports.get_ticket_price( start, end )
         distance = self.all_airports.distance_between_two_airports( start, end )
         aircraft = self.air_lines.get_aircraft_by_distance( distance )
-        return [aircraft, price]
+        trip = Trip([start, end], aircraft, price, start_date, [ start, end ] )
+        return trip
 
-    def set_trip_one_city_two_way( self ):
-        pass 
-    
-    def set_trip_multi_city_one_way( self ):
-        pass 
+    """ 
+        Trip Info : [ city1, city2 , city3 ]
+    """
+    def set_trip_two_city_one_way( self, trip_info, start_date ):
+        trip1 = self.set_trip_one_city_one_way( trip_info[0],trip_info[1], start_date )
+        trip2 = self.set_trip_one_city_one_way( trip_info[1],trip_info[2], start_date )
+        return [ trip1, trip2 ]
+    """ 
+        Trip Info : [ city1, city2, city3, city4, city5 ]
+    """
+    def set_trip_multi_city_one_way_fixed_route( self, trip_info, start_date ):
+        trips = []
+        for i in range( 0, len(trip_info)-1 ):
+            trip = self.set_trip_one_city_one_way( trip_info[i], trip_info[i+1], start_date)
+            trips.append(trip)
+        return trips
     
     def set_trip_multi_city_round( self ):
         pass 
--- module21/main.py
+++ module21/main.py
@@ -3,8 +3,8 @@ from TravelAgent import TravelAgent
 def main():
     travel_agent = TravelAgent('Zulzana Travel')
     trip_info1 = travel_agent.set_trip_one_city_one_way('DAC', 'PRA', '05/07/2022')
-    print('Aircraft ',trip_info1[0])
-    print('Price ', trip_info1[1])
+    print('Aircraft ',trip_info1.aircraft)
+    print('Price ', trip_info1.price)
 
 if __name__ == "__main__":
     main()
