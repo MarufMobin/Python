ad51bb1977382fcc473dfdcfeaa22d447c2b42ea
Maruf Mobin <mobinh264@gmail.com>
2022-11-15 20:22:00 +0600

    21-4 Get Aircraft and price for one city one way trip

--- module21/TravelAgent.py
+++ module21/TravelAgent.py
@@ -1,10 +1,29 @@
+from All_Airports import AllAirports
+from Air_Lines import AirLines
+
 class TravelAgent:
     def __init__(self, name) -> None:
         self.name = name
         self.trips = None
+        self.all_airports = AllAirports()
+        self.air_lines = AirLines()
 
-    def set_trip_one_city_one_way( self ):
-        pass 
+    """ 
+        Params :
+        Start : Strating City Code
+        End : Destination City Code 
+        Starting Date : Date When You Want to Start the trip
+
+        return : 
+        aircraft , price
+
+        Notes : use Aircraft to select aircraft 
+    """
+    def set_trip_one_city_one_way( self, start, end, start_date ):
+        price = self.all_airports.get_ticket_price( start, end )
+        distance = self.all_airports.distance_between_two_airports( start, end )
+        aircraft = self.air_lines.get_aircraft_by_distance( distance )
+        return [aircraft, price]
 
     def set_trip_one_city_two_way( self ):
         pass 
