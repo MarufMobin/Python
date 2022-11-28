ba0a186b109d8a267b0d58e91f9081f0082008ed
Maruf Mobin <mobinh264@gmail.com>
2022-11-28 21:19:09 +0600

    29-1 Simple Numpy array zeros ones arange linspace

---
+++ module29/numpy_basic.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+py_list = [ 1, 2, 35, 65, 45 ]
+np_array = np.array( [ 1, 5, 6, 48, 6 ] )
+ten_zeros = np.zeros(10)
+fifteen_ones = np.ones(15)
+sequence = np.arange(16)
+stepper = np.arange(0,51,5)
+spaced = np.linspace( 0, 10, num=5)
+# print(py_list)
+# print(np_array)
+# print(ten_zeros)
+# print(fifteen_ones)
+# print(sequence)
+# print(stepper)
+print(spaced)
