9ca4c8a820a2787d1f153b00950e2246de39b167
Maruf Mobin <mobinh264@gmail.com>
2022-11-29 20:22:01 +0600

    29-7 Display Confusion matrix and plot confusion matrix

--- module29/logistic_regression.py
+++ module29/logistic_regression.py
@@ -1,6 +1,8 @@
 from sklearn.datasets import load_digits
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
 import matplotlib.pyplot as plt 
 
 digits = load_digits()
@@ -21,10 +23,21 @@ X_train, X_test, Y_train, Y_test = train_test_split( X, Y , test_size=0.20 )
 
 model = LogisticRegression()
 model.fit( X_train, Y_train)
+
+# Manual Spot Testing 
+""" 
 print('target of the value of the test ', digits.target[1700])
-
 result = model.predict([digits.data[1700]])
-print('test result', result)
+print('test result', result) 
 
+"""
+# Cheking Accuracy 
+accuracy = model.score( X_test, Y_test )
+# print('Model Accuracy', accuracy )
 
-
+Y_predicted = model.predict( X_test )
+confusion = confusion_matrix( Y_test, Y_predicted )
+# print(confusion)
+# Using Plot for Confusion matrix 
+plot_confusion_matrix( model, X_test, Y_test )
+plt.show()
