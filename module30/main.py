d4220219a20e3c12c238f6e812777123710aa080
Maruf Mobin <mobinh264@gmail.com>
2022-11-30 13:20:56 +0600

    30-5 Run Logistics Model to see whether someone will left the company or not

--- module30/main.py
+++ module30/main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split 
+
 
 datas = pd.read_csv('HR_comma_sep.csv')
 # Step 1 : -> print all Data 
@@ -42,13 +45,29 @@ marged = pd.concat( [ datas, dummies ], axis='columns' )
 final_data = marged.drop(['Department', 'technical'], axis='columns')
 # print( 'Department' in list(final_data.columns))
 # print( 'Department' in list(marged.columns))
-
 # print(list(final_data.columns))
 
+# Step 9 -> Plot data set to see the data relation 
+
 # plt.scatter( x = final_data.salary, y = final_data.left )
-
 # plt.scatter( x = final_data.satisfaction_level, y = final_data.left )
+# plt.scatter( x = final_data.time_spend_company, y = final_data.left )
+# plt.show()
 
-plt.scatter( x = final_data.time_spend_company, y = final_data.left )
+# Step 10 -> Draw Model 
+X = final_data.drop('left', axis='columns')
+y = final_data.left 
 
-plt.show()
+X_train , X_test , y_train, y_test  = train_test_split( X, y , test_size=0.2 )
+
+model = LogisticRegression()
+model.fit( X_train , y_train )
+
+accuracy = model.score( X_test , y_test )
+# print(accuracy)
+
+# print( list( X.columns ) )
+
+result = model.predict([ [ 0.85, 0.87, 6, 232, 5, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0 ] ])
+
+# print( result )
