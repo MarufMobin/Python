1b060a1419e6d5812e6e514cc5c1f9d876abc1b2
Maruf Mobin <mobinh264@gmail.com>
2022-12-02 03:46:51 +0600

    Convert the following recursive functions into iterative ones

--- module31/usingLoopProgram.py
+++ module31/usingLoopProgram.py
@@ -1,2 +1,38 @@
+""" 
+    Sumation of all odd number 
+"""
 def sum_odd( n, total ):
-    pass 
+    for i in range( n ):
+        if i % 2 == 1:
+            total += i
+    return total
+
+result = sum_odd( 22, 0 )
+print(result)
+
+""" 
+    Find a max number in a list 
+"""
+def max( l, n ):
+    for i in range( len( l ) ):
+        if l[i] > n:
+            n = l[i] 
+    return n    
+
+l = [ 111, 3, 5, 203, 43, 13, 53, 54 ]
+n = 0
+max_number = max( l , n )
+print(max_number)
+
+""" 
+    find a list lenght
+"""
+def mylen( l, n ):
+    for i in range( len(l) ):
+        n += 1
+    return n
+
+l = [ 2, 3, 4, 54, 5, 32, 45, 6, 83 ]
+n = 0
+listLen = mylen( l, n )
+print( listLen )
